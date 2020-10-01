eedc0ecee9f305b3bfff05ca3df61cfd52a20dd2
Zaeema Anwar <thezaeemaanwar@gmail.com>
2020-10-01 17:26:12 +0500

    Added search filters and a homepage

--- src/App.js
+++ src/App.js
@@ -6,21 +6,31 @@ const App = () => {
   const APP_ID = "23b19a81";
   const APP_KEY = "10d454b3c2543dcdf8e1858b3e1b8f51";
 
+  // The Recepies Fetched
   const [Recipes, setRecipes] = useState([]);
+  // The value tyoed in search
   const [Search, setSearch] = useState("");
+  // Complete Search
   const [Query, setQuery] = useState("");
+  // Health Type
+  const [Health, setHealth] = useState("alcohol-free");
+  // Diet Type
+  const [Diet, setDiet] = useState("balanced");
 
+  // The rendering function
   useEffect(() => {
     (async () => getRecepies())();
   }, [Query]);
 
-  const exampleRed = `https://api.edamam.com/search?q=${Query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+  const exampleRed = `https://api.edamam.com/search?q=${Query}&app_id=${APP_ID}&app_key=${APP_KEY}&diet=${Diet}&health=${Health}`;
 
+  // Fetch data from the API and set the state of Recipe to ir
   const getRecepies = async () => {
     const response = await fetch(exampleRed);
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits);
+    console.log("Health = ", Health);
+    console.log("data = ", data.hits);
   };
 
   const updateSearch = (e) => {
@@ -30,6 +40,9 @@ const App = () => {
   const getSearch = (e) => {
     e.preventDefault();
     setQuery(Search);
+    setHealth(document.getElementById("health").value);
+    setDiet(document.getElementById("diet").value);
+    console.log("Health in getSearch = ", Health);
     setSearch("");
   };
 
@@ -39,28 +52,50 @@ const App = () => {
 
   return (
     <div className="App">
+      <h1 className="header">Welcome to fooderia</h1>
+      <h3 className="description">
+        Get the recipes of your favorite foods Type in the Search bar what you
+        want to make alongwith the calories and food-type{" "}
+      </h3>
       <form onSubmit={getSearch} className="search-form">
+        <select className="dropdown" id="health">
+          <option value="alcohol-free">Alcohol Free</option>
+          <option value="tree-nut-free">Tree Nut free</option>
+          <option value="peanut-free">Peanut Free</option>
+          <option value="sugar-conscious">Sugar Conscious</option>
+          <option value="vegan">Vegan</option>
+          <option value="vegetarian"> Vegetarian</option>
+        </select>
+        <select className="dropdown" id="diet">
+          <option value="balanced">Balanced</option>
+          <option value="high-protein">High Protein</option>
+          <option value="low-carb">Low Crab</option>
+          <option value="low-fat">Low Fat</option>
+        </select>
+
         <input
           className="search-bar"
           type="text"
           value={Search}
           onChange={updateSearch}
         />
-        <button className="search-button" type="submit">
+        <a href="#head" className="search-button" type="submit ">
           Search
-        </button>
+        </a>
       </form>
-      <h1 className = "heading" >{capitalize(Query)} Recipes</h1>
-      <div className="recipes">
-        {Recipes.map((recipe) => (
-          <Recipe
-            key={recipe.recipe.label}
-            title={recipe.recipe.label}
-            calories={recipe.recipe.calories}
-            image={recipe.recipe.image}
-            ingredients={recipe.recipe.ingredients}
-          />
-        ))}
+      <div id="head">
+        <h1 className="heading">{capitalize(Query)} Recipes</h1>
+        <div className="recipes">
+          {Recipes.map((recipe) => (
+            <Recipe
+              key={Recipes.indexOf(recipe)}
+              title={recipe.recipe.label}
+              calories={recipe.recipe.calories}
+              image={recipe.recipe.image}
+              ingredients={recipe.recipe.ingredients}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
