943be7ebf0a42c7b0d39e2ecb10dec5e08436fcf
Zaeema Anwar <thezaeemaanwar@gmail.com>
2020-10-01 18:43:51 +0500

    Updates

---
+++ precache-manifest.2bc1df462a47dc018715bf10f7d54867.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621bf36673f011e485c5a55da00baea4",
+    "url": "/thezaeemaanwar.github.io/Fooderia/index.html"
+  },
+  {
+    "revision": "d7f0aef1753ce5bd4733",
+    "url": "/thezaeemaanwar.github.io/Fooderia/static/css/main.a9a22ca8.chunk.css"
+  },
+  {
+    "revision": "4d605e5a8f262afd0ba7",
+    "url": "/thezaeemaanwar.github.io/Fooderia/static/js/2.b7bffcd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thezaeemaanwar.github.io/Fooderia/static/js/2.b7bffcd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f0aef1753ce5bd4733",
+    "url": "/thezaeemaanwar.github.io/Fooderia/static/js/main.78470f28.chunk.js"
+  },
+  {
+    "revision": "96cd0e5110ed5c86e25c",
+    "url": "/thezaeemaanwar.github.io/Fooderia/static/js/runtime-main.47943460.js"
+  },
+  {
+    "revision": "a0bb8995539ef4946eef46cf1219c304",
+    "url": "/thezaeemaanwar.github.io/Fooderia/static/media/bg.a0bb8995.jpg"
+  }
+]);
